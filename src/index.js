b8f4327c125045596798115b7d665be2f135f165
LukeBrutale <yuriybema@gmail.com>
2023-05-22 21:20:48 +0300

    commit

--- src/index.js
+++ src/index.js
@@ -5,13 +5,9 @@ import App from './App';
 import state from './redux/state';
 import { addPost } from './redux/state';
 
-// import reportWebVitals from './reportWebVitals';
-
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <App state={state} addPost={addPost} />
   </React.StrictMode>,
 );
-
-// reportWebVitals();
